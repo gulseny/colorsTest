6bbb58122e4524199fc9436fcda740766a639195
Gulsen Yilmaz <gsenol@gmail.com>
2014-04-01 15:02:50 -0700

    enabled ng routes and created play view

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,17 @@
-angular.module('colorsTest', [])
-.controller('mainController', function($scope){
+angular.module('colorsTest', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: '../views/play.html'
+	})
+	.otherwise('/gameOver', {
+		template: '<div>Game Over!</div>'
+	});
+}])
+.controller('playController', function($scope){
 	$scope.person = {
 		name: 'Gulsen'
 	}
-})
+
+});
+
